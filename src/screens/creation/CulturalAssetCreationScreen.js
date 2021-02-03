ee2ee3245454f8a0e0205885cf8ceda53a94c05b
Gustav <uszyc@student.kit.edu>
2021-02-03 18:00:08 +0100

    Add scaffold to CA-CreationScreen and CA-DetailScreen

--- src/screens/creation/CulturalAssetCreationScreen.js
+++ src/screens/creation/CulturalAssetCreationScreen.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Scaffold from '../../components/baseComponents/Scaffold';
 
-export default function CulturalAssetCreationScreen() {
-  return <Text>Here you can create a cultural asset</Text>;
+export default function CulturalAssetCreationScreen({navigation, route}) {
+  return (
+    <Scaffold
+      navigation={navigation}
+      content={
+        <Text>
+          Here you can create a cultural asset
+        </Text>
+      }
+    />
+  ); 
 }
--- src/screens/detail/CulturalAssetDetailScreen.js
+++ src/screens/detail/CulturalAssetDetailScreen.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Scaffold from '../../components/baseComponents/Scaffold';
 
-export default function CulturalAssetDetailScreen({route}) {
-  return <Text>Cultural Asset Detail Screen for id: {route.params.id}</Text>;
+export default function CulturalAssetDetailScreen({navigation, route}) {
+  return (
+    <Scaffold
+      navigation={navigation}
+      content={
+        <Text>
+          Cultural Asset Detail Screen for id: {route.params.id}
+        </Text>
+      }
+    />
+  );
+  
 }
