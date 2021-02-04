3252c0026df688b24a42da50c2dc3ed82aab3598
Gustav <uszyc@student.kit.edu>
2021-02-04 21:36:26 +0100

    Style CulturalAssetCreationScreen

--- src/screens/creation/CulturalAssetCreationScreen.js
+++ src/screens/creation/CulturalAssetCreationScreen.js
@@ -1,6 +1,124 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Button, Chip, HelperText, TextInput} from 'react-native-paper';
+import Scaffold from '../../components/baseComponents/Scaffold';
 
 export default function CulturalAssetCreationScreen({navigation, route}) {
-  return <Text>Here you can create a cultural asset</Text>;
+  const [culturalAsset, setCulturalAsset] = React.useState({
+    name: '',
+    description: '',
+    tags: [],
+    comments: [{}],
+    media: [{}],
+    label: '',
+    longitude: 0.0,
+    latitude: 0.0,
+    level: 0,
+    parent: {},
+    children: [],
+    tasks: [{}],
+  });
+
+  const [address, setAddress] = React.useState('');
+  const [priority, setPriority] = React.useState(0);
+
+  const onChangeName = (name) => {
+    const updatedCulturalAsset = {culturalAsset};
+    updatedCulturalAsset.name = name;
+    setCulturalAsset(updatedCulturalAsset);
+  };
+  const onChangeDescription = (description) => {
+    const updatedCulturalAsset = {culturalAsset};
+    updatedCulturalAsset.description = description;
+    setCulturalAsset(updatedCulturalAsset);
+  };
+  const onChangeAddress = (address) => setAddress(address);
+  const onChangePeculiarity = (peculiarity) => {
+    const updatedCulturalAsset = {culturalAsset};
+    updatedCulturalAsset.label = peculiarity;
+    setCulturalAsset(updatedCulturalAsset);
+  };
+  const onChangeParent = (parentId) => {
+    const updatedCulturalAsset = {culturalAsset};
+    updatedCulturalAsset.parent = {id: parentId};
+    setCulturalAsset(updatedCulturalAsset);
+  };
+
+  const finishCreation = () => navigation.goBack();
+
+  return (
+    <Scaffold>
+      <SafeAreaView>
+        <ScrollView>
+          <TextInput
+            label="Name"
+            value={culturalAsset.name}
+            onChangeText={onChangeName}
+          />
+          <TextInput
+            label="Beschreibung"
+            value={culturalAsset.description}
+            onChangeText={onChangeDescription}
+          />
+          <TextInput
+            label="Adresse"
+            value={address}
+            onChangeText={onChangeAddress}
+          />
+
+          <TextInput
+            label="Obergruppe"
+            value={culturalAsset.parent.id}
+            onChangeParent={onChangeParent}
+            style={{marginTop: 25}}
+          />
+          <TextInput
+            label="Besonderheiten"
+            value={culturalAsset.label}
+            onChangeText={onChangePeculiarity}
+          />
+          <View style={{flex: 1, marginTop: 25, marginBottom: 25}}>
+            <Text style={{marginLeft: 10}}>Wähle Priorität:</Text>
+            {priorities.map((prio, index) => {
+              return (
+                <View
+                  style={{
+                    margin: 2,
+                    flexWrap: 'wrap',
+                  }}>
+                  <Chip
+                    icon="alert-circle"
+                    key={prio}
+                    mode="flat"
+                    height={30}
+                    selected={index === priority}
+                    onPress={() => setPriority(index)}>
+                    {prio}
+                  </Chip>
+                </View>
+              );
+            })}
+          </View>
+          <Button icon="camera" mode="contained" style={{marginBottom: 20}}>
+            Füge Medien hinzu
+          </Button>
+          <Button
+            icon="check"
+            mode="contained"
+            onPress={finishCreation}
+            style={{marginBottom: 50}}>
+            Fertig
+          </Button>
+        </ScrollView>
+      </SafeAreaView>
+    </Scaffold>
+  );
 }
+
+const priorities = [
+  'Keine Priorität',
+  'Geringe Priorität',
+  'Normale Priorität',
+  'Hohe Priorität',
+  'Höchste Priorität',
+];
--- src/screens/list/CulturalAssetListScreen.js
+++ src/screens/list/CulturalAssetListScreen.js
@@ -3,10 +3,14 @@ import {FancyList} from '@ilt-pse/react-native-kueres';
 import Scaffold from '../../components/baseComponents/Scaffold';
 import CulturalAssetListItem from '../../components/listItems/CulturalAssetListItem';
 import {culturalAssetData} from '../../../testdata';
+import {Button} from 'react-native-paper';
 
 export default function CulturalAssetListScreen({navigation}) {
+  const goAssetCreation = () => navigation.push('CulturalAssetCreationScreen');
+
   return (
     <Scaffold>
+      <Button icon="plus-circle-outline" onPress={goAssetCreation} />
       <FancyList
         title="Kulturgüter"
         data={culturalAssetData}
