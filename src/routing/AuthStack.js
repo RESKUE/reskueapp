4cb03412262d6d370897490ace89b48dc23f57cb
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-04 19:37:28 +0100

    Hide the login screens header

--- src/routing/AuthStack.js
+++ src/routing/AuthStack.js
@@ -6,7 +6,7 @@ const Stack = createStackNavigator();
 
 export default function AuthStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
     </Stack.Navigator>
   );
