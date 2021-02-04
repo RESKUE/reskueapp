002dae2c9687353f653cbe786d0595e3b5aad6b9
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-04 17:49:59 +0100

    Use the FancyList on the NotificationListScreen

--- src/components/listItems/NotificationListItem.js
+++ src/components/listItems/NotificationListItem.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {List} from 'react-native-paper';
 
-export default function NotificationListItem({entity}) {
+export default function NotificationListItem({data}) {
+  function onPress() {
+    console.log('Goto notification', data.id);
+  }
+
   return (
-    <View
-      style={{
-        backgroundColor: '#f9c2ff',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-      }}>
-      <Text onPress={() => entity.id++} style={{fontSize: 24}}>
-        {entity.title} {entity.id}
-      </Text>
-    </View>
+    <List.Item
+      key={data.id}
+      title={data.title}
+      description={data.message}
+      onPress={onPress}
+    />
   );
 }
--- src/screens/list/NotificationListScreen.js
+++ src/screens/list/NotificationListScreen.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FancyList} from '@ilt-pse/react-native-kueres';
+import Scaffold from '../../components/baseComponents/Scaffold';
+import NotificationListItem from '../../components/listItems/NotificationListItem';
 
 export default function NotificationListScreen({navigation}) {
-  return <Text>Here you can see all your notifications.</Text>;
+  return (
+    <Scaffold>
+      <FancyList
+        title="Benachrichtigungen"
+        data={notificationData}
+        component={NotificationListItem}
+      />
+    </Scaffold>
+  );
 }
+
+const notificationData = [
+  {
+    id: 1,
+    title: 'Waldbrand',
+    message: 'Nähe Wildparkstadion, Berufsfeuerwehr unterwegs',
+  },
+  {
+    id: 2,
+    title: 'Kernschmelze Campus Nord',
+    message: 'Wichtig: Den Studenten nicht frei geben',
+  },
+];
