83287dc824a45f305665d9c62cc02865473561d6
Gustav <uszyc@student.kit.edu>
2021-02-08 18:46:41 +0100

    Use CA-model for CA-ListItem

--- src/components/listItems/CulturalAssetListItem.js
+++ src/components/listItems/CulturalAssetListItem.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {List} from 'react-native-paper';
+import CulturalAsset from '../../models/CulturalAsset';
 
 export default function CulturalAssetListItem({data}) {
   const navigation = useNavigation();
+  const culturalAsset = new CulturalAsset(data);
 
   function onPress() {
-    navigation.push('CulturalAssetDetailScreen', {id: data.id});
+    navigation.push('CulturalAssetDetailScreen', {id: culturalAsset.data.id});
   }
 
   return (
     <List.Item
-      key={data.id}
-      title={data.name}
-      description={data.description}
+      key={culturalAsset.data.id}
+      title={culturalAsset.data.name}
+      description={culturalAsset.data.description}
       onPress={onPress}
     />
   );
