9d359841939bc1118b0eeaa42ef667ede076a609
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-14 06:47:58 +0100

    Simplify the MediaListItem

--- src/components/listItems/mediaListItems/MediaListItem.js
+++ src/components/listItems/mediaListItems/MediaListItem.js
@@ -3,12 +3,14 @@ import {useNavigation} from '@react-navigation/native';
 import {StyleSheet, View, Text} from 'react-native';
 import {useTheme, TouchableRipple} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {MediaType} from "@ilt-pse/react-native-kueres";
+import {MediaType} from '@ilt-pse/react-native-kueres';
 
 export default function MediaListItem({data}) {
   const navigation = useNavigation();
   const {colors} = useTheme();
   const bg = {backgroundColor: colors.primary};
+  const type = MediaType.fromMime(data.mimeType);
+  const icon = MediaType.nameIcon(type);
 
   function onPress() {
     navigation.push('MediaDetailScreen', {id: data.id});
@@ -17,11 +19,7 @@ export default function MediaListItem({data}) {
   return (
     <TouchableRipple style={[styles.item, bg]} onPress={onPress}>
       <View>
-        <Icon
-          style={styles.half}
-          name={getIconFromMime(data.mimeType)}
-          size={36}
-        />
+        <Icon style={styles.half} name={icon} size={36} />
         <Text style={styles.half}>{data.altText}</Text>
       </View>
     </TouchableRipple>
@@ -42,17 +40,3 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
   },
 });
-
-function getIconFromMime(mime) {
-  const type = MediaType.fromMime(mime);
-  switch (type) {
-    case MediaType.image:
-      return 'file-image-outline';
-    case MediaType.video:
-      return 'file-video-outline';
-    case MediaType.audio:
-      return 'file-music-outline';
-    default:
-      return 'file-question-outline';
-  }
-}
