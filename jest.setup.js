45394994ddba68267f0bd79063c555822cf557c4
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-20 19:40:15 +0100

    Configure jest to mock timeouts

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,8 @@
 import mockReactNativeReanimated from 'react-native-reanimated/mock';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
+jest.useFakeTimers();
+
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native-reanimated', () => mockReactNativeReanimated);
 jest.mock('react-native-tab-view', () => {
