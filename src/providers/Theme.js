83a1e2814c4a88f9c47553d5ad4d5c6293d5bf73
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-23 15:32:45 +0100

    Add the redish reskue color to the theme

--- src/providers/Theme.js
+++ src/providers/Theme.js
@@ -1,12 +1,14 @@
 import {DefaultTheme} from 'react-native-paper';
 
 const RESKUE_GREEN = '#01A569';
+const RESKUE_REDISH = '#a51d01';
 
 export default {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
     primary: RESKUE_GREEN,
+    redish: RESKUE_REDISH,
     highlightBG: '#02d487',
     highlightFG: '#ffffff',
   },
