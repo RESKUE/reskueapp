a619726fabbc24572cc697d5f27b175c4453f60f
Gustav <uszyc@student.kit.edu>
2021-02-25 15:33:00 +0100

    Delete unused requests

--- src/handlers/TaskCreationHook.js
+++ src/handlers/TaskCreationHook.js
@@ -8,12 +8,6 @@ export default function useTaskCreation() {
   const {client: taskClient, result: taskResult} = useClient({
     authenticated: true,
   });
-  const {client: subtaskPostClient, result: subtaskPostResult} = useClient({
-    authenticated: true,
-  });
-  const {client: subtaskDeleteClient, result: subtaskDeleteResult} = useClient({
-    authenticated: true,
-  });
 
   const postTask = React.useCallback(
     async (task) => {
@@ -30,36 +24,8 @@ export default function useTaskCreation() {
     [taskClient],
   );
 
-  const postSubtask = React.useCallback(
-    async (subtask) => {
-      const url = appConfig.rest.baseUrl + '/subtask';
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(subtask),
-      };
-      await subtaskPostClient.request(url, options, policy);
-    },
-    [subtaskPostClient],
-  );
-
-  const requestSubtaskDeletion = React.useCallback(
-    async (subtaskId) => {
-      const url = appConfig.rest.baseUrl + `/subtask/${subtaskId}`;
-      const options = {method: 'DELETE'};
-      await subtaskDeleteClient.request(url, options, policy);
-    },
-    [subtaskDeleteClient],
-  );
-
   return {
     taskResult,
-    subtaskPostResult,
-    subtaskDeleteResult,
     postTask,
-    postSubtask,
-    requestSubtaskDeletion,
   };
 }
