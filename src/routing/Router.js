372047d22550c44c9500a5810b5ff4579f5eac4d
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-25 17:14:04 +0100

    Show a loading indicator during intermediate auth states

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {AuthContext} from '@ilt-pse/react-native-kueres';
+import {AuthContext, LoadingIndicator} from '@ilt-pse/react-native-kueres';
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
 
 export default function Router() {
   const {authStatus} = React.useContext(AuthContext);
+
+  if (authStatus === null) {
+    return <LoadingIndicator />;
+  }
 
   return (
     <NavigationContainer>
