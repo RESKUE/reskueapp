20b090b3a729539c3c13c8c4db4cb970705d47d3
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-26 02:57:59 +0100

    Make the notification creation screen reachable from the header

---
+++ src/components/AlarmAction.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import useRoles from '../handlers/RolesHook';
+import {useNavigation} from '@react-navigation/native';
+import {Appbar, useTheme} from 'react-native-paper';
+
+export default function AlarmAction() {
+  const navigation = useNavigation();
+  const {isAdmin} = useRoles();
+  const {colors} = useTheme();
+
+  if (!isAdmin) {
+    // Preserve layout using an invisible placeholder.
+    return <Appbar.Action />;
+  }
+
+  return (
+    <Appbar.Action
+      icon="bell"
+      color={colors.redish}
+      onPress={launchNotificationCreation}
+    />
+  );
+
+  function launchNotificationCreation() {
+    navigation.push('NotificationCreationScreen');
+  }
+}
--- src/components/baseComponents/HeaderBar.js
+++ src/components/baseComponents/HeaderBar.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {StyleSheet, Image} from 'react-native';
 import {Appbar} from 'react-native-paper';
+import AlarmAction from '../AlarmAction';
 
-export default function HeaderBar({navigation}) {
-  const createAlarm = () => navigation.navigate();
-
+export default function HeaderBar() {
   return (
     <Appbar.Header style={styles.header}>
       <Appbar.Action />
@@ -18,7 +17,7 @@ export default function HeaderBar({navigation}) {
         }
         style={styles.content}
       />
-      <Appbar.Action icon="bell" color="#a51d01" onPress={createAlarm} />
+      <AlarmAction />
     </Appbar.Header>
   );
 }
