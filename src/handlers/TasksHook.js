440d7d0c4b18a7fa88f5e601308856074829ad4d
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-26 07:58:11 +0100

    Show real data on the my task list screen

--- src/handlers/TasksHook.js
+++ src/handlers/TasksHook.js
@@ -4,14 +4,22 @@ import appConfig from '../../app.json';
 
 const policy = FetchPolicy.cacheAndNetwork;
 const options = {method: 'GET'};
-const url = appConfig.rest.baseUrl + '/task';
 
 export default function useTasks() {
   const {client, result} = useClient({authenticated: true});
 
   const requestTasks = React.useCallback(async () => {
+    const url = appConfig.rest.baseUrl + '/task';
     await client.request(url, options, policy);
   }, [client]);
 
-  return {result, requestTasks};
+  const requestUserTasks = React.useCallback(
+    async (userId) => {
+      const url = `${appConfig.rest.baseUrl}/user/${userId}/helperTasks`;
+      await client.request(url, options, policy);
+    },
+    [client],
+  );
+
+  return {result, requestTasks, requestUserTasks};
 }
--- src/screens/list/MyTaskListScreen.js
+++ src/screens/list/MyTaskListScreen.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import {FancyList} from '@ilt-pse/react-native-kueres';
+import {FancyList, LoadingIndicator} from '@ilt-pse/react-native-kueres';
 import Scaffold from '../../components/baseComponents/Scaffold';
 import MyTaskListItem from '../../components/listItems/MyTaskListItem';
-import {taskData} from '../../../testdata';
+import useTasks from '../../handlers/TasksHook';
+import {useFocusEffect} from '@react-navigation/native';
 
 export default function MyTaskListScreen() {
+  const {result, requestUserTasks} = useTasks();
+  const content = result?.data?.content;
+
+  useFocusEffect(
+    React.useCallback(() => {
+      requestUserTasks();
+    }, [requestUserTasks]),
+  );
+
+  if (!result) {
+    return <LoadingIndicator />;
+  }
+
   return (
     <Scaffold>
       <FancyList
         title="Meine Aufgaben"
-        data={taskData}
+        data={content ?? []}
         component={MyTaskListItem}
       />
     </Scaffold>
