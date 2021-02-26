247db44af1fcb4938f5c232ee8281a072c063f1c
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-02-26 05:04:00 +0100

    Visualize a assets priority and endangered status

--- src/components/listItems/CulturalAssetListItem.js
+++ src/components/listItems/CulturalAssetListItem.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {List} from 'react-native-paper';
-import CulturalAsset from '../../models/CulturalAsset';
+import {List, useTheme} from 'react-native-paper';
 
 export default function CulturalAssetListItem({data}) {
   const navigation = useNavigation();
-  const culturalAsset = new CulturalAsset(data);
-
-  function onPress() {
-    navigation.push('CulturalAssetDetailScreen', {id: culturalAsset.data.id});
-  }
+  const {colors} = useTheme();
+  const priority = data?.priority;
+  const isEndangered = data?.isEndangered ?? false;
+  const iconName = getPriorityIcon();
+  const iconColor = getIconColor();
 
   return (
     <List.Item
-      key={culturalAsset.data.id}
-      title={culturalAsset.data.name}
-      description={culturalAsset.data.description}
+      key={data.id}
+      right={(props) => (
+        <List.Icon {...props} icon={iconName} color={iconColor} />
+      )}
+      title={data.name}
+      description={data.description}
       onPress={onPress}
     />
   );
+
+  function onPress() {
+    navigation.push('CulturalAssetDetailScreen', {id: data.id});
+  }
+
+  function getIconColor() {
+    return isEndangered ? colors.redish : colors.primary;
+  }
+
+  function getPriorityIcon() {
+    if (Number.isInteger(priority) && priority >= 0 && priority <= 9) {
+      return `numeric-${priority}-circle`;
+    }
+    return 'circle';
+  }
 }
