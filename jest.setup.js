b3f7ab102f2ac60ddc2f4700ce80aed43933316c
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-12 02:45:31 +0100

    Mock react navigation hooks and top tab nav

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,4 @@
+import React from "react";
 import mockReactNativeReanimated from 'react-native-reanimated/mock';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
@@ -8,4 +9,26 @@ jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native-reanimated', () => mockReactNativeReanimated);
 jest.mock('react-native-tab-view', () => {
   return {Pager: {}, TabView: {}};
+});
+
+jest.mock('@react-navigation/native', () => {
+  const actual = jest.requireActual('@react-navigation/native');
+  return {
+    ...actual,
+    useFocusEffect: () => jest.fn(),
+    useNavigation: () => ({
+      navigate: jest.fn(),
+    }),
+  };
+});
+
+jest.mock("@react-navigation/material-top-tabs", () => {
+  const actual = jest.requireActual("@react-navigation/material-top-tabs");
+  return {
+    ...actual,
+    createMaterialTopTabNavigator: jest.fn().mockReturnValue({
+      Navigator: ({children}) => <>{children}</>,
+      Screen: ({children}) => <>{children}</>,
+    }),
+  }
 });
