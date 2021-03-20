9d010840e2d9a0e858fc657ffd0e960b538651f6
Gustav <uszyc@student.kit.edu>
2021-03-20 04:24:40 +0100

    Await deletionResult instead of using useEffect

--- src/screens/detail/TaskDetailScreen.js
+++ src/screens/detail/TaskDetailScreen.js
@@ -88,17 +88,6 @@ export default function TaskDetailScreen({navigation, route}) {
     [taskDataExpression],
   );
 
-  //If deletion was successful go back
-  React.useEffect(() => {
-    if (deletionResult) {
-      if (deletionResult.data?.deleted) {
-        navigation.goBack();
-      } else {
-        console.log(deletionResult);
-      }
-    }
-  }, [deletionResult, navigation]);
-
   const onChangeSubtaskState = (subtaskId) => {
     const updatedTask = new Task(task.data);
     const index = updatedTask.data.subtasks.findIndex(
