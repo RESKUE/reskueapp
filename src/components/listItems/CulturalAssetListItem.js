c2c9ea16359a2a28cb1f170afebe567322ab2100
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-20 23:59:13 +0100

    Mark endangered assets with an colored icon

--- src/components/listItems/CulturalAssetListItem.js
+++ src/components/listItems/CulturalAssetListItem.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import {View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {List, TouchableRipple} from 'react-native-paper';
+import {List, TouchableRipple, useTheme} from 'react-native-paper';
 import AssetTags from '../AssetTags';
 
 export default function CulturalAssetListItem({data}) {
   const navigation = useNavigation();
+  const {colors} = useTheme();
 
   return (
     <TouchableRipple key={data.id} onPress={onPress}>
       <View>
-        <List.Item title={data.name} description={data.description} />
+        <List.Item
+          title={data.name}
+          description={data.description}
+          right={getIcon}
+        />
         <AssetTags data={data} />
       </View>
     </TouchableRipple>
   );
+
+  function getIcon(props) {
+    if (!data.isEndangered) {
+      return null;
+    }
+    return <List.Icon {...props} icon="alert" color={colors.redish} />;
+  }
 
   function onPress() {
     navigation.push('CulturalAssetDetailScreen', {id: data.id});
