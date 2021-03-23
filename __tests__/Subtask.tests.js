e129e582d870dd840f9de1ffbe530306353fea0a
Gustav <uszyc@student.kit.edu>
2021-03-23 17:35:29 +0100

    Delete unused subtask

--- __tests__/Subtask.tests.js
+++
@@ -1,7 +0,0 @@
-import Subtask from '../src/models/Subtask';
-
-it('is complete if state is 0', () => {
-  const data = {state: 0};
-  const subtask = new Subtask(data);
-  expect(subtask.isIncomplete()).toBe(true);
-});
