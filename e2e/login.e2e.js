6fa64a19de205c3560194e00cf3fcb1f700cae83
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-24 04:20:18 +0100

    Include e2e tests in linting and formatting

--- e2e/login.e2e.js
+++ e2e/login.e2e.js
@@ -10,22 +10,22 @@ describe('Login flow', () => {
   it('should login admin successfully', async () => {
     await expect(element(by.id('loginScreenUsernameInput'))).toBeVisible();
 
-    await element(by.id("loginScreenUsernameInput")).typeText("admin");
-    await element(by.id("loginScreenPasswordInput")).typeText("1234");
-    await element(by.id("loginScreenLoginButton")).tap();
+    await element(by.id('loginScreenUsernameInput')).typeText('admin');
+    await element(by.id('loginScreenPasswordInput')).typeText('1234');
+    await element(by.id('loginScreenLoginButton')).tap();
 
     await expect(element(by.id('alarmActionBell'))).toBeVisible();
-    await element(by.id("navigationBarLogoutButton")).tap();
+    await element(by.id('navigationBarLogoutButton')).tap();
   });
 
   it('should login helper successfully', async () => {
     await expect(element(by.id('loginScreenUsernameInput'))).toBeVisible();
 
-    await element(by.id("loginScreenUsernameInput")).typeText("helper");
-    await element(by.id("loginScreenPasswordInput")).typeText("1234");
-    await element(by.id("loginScreenLoginButton")).tap();
+    await element(by.id('loginScreenUsernameInput')).typeText('helper');
+    await element(by.id('loginScreenPasswordInput')).typeText('1234');
+    await element(by.id('loginScreenLoginButton')).tap();
 
     await expect(element(by.text('Kulturgüter'))).toBeVisible();
-    await element(by.id("navigationBarLogoutButton")).tap();
+    await element(by.id('navigationBarLogoutButton')).tap();
   });
 });
