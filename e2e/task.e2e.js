b11b983034bc1229c8548ad2602c3a0add0e089f
Youssef Taieb <ubghs@student.kit.edu>
2021-03-27 18:27:04 +0100

    Update the task E2E tests

--- e2e/task.e2e.js
+++ e2e/task.e2e.js
@@ -21,10 +21,10 @@ describe('Login flow', () => {
 
     // Enter asset details
     await element(by.id('assetCreationScreenNameInput')).typeText(
-      'Mona Lisa\n',
+      'test asset\n',
     );
     await element(by.id('assetCreationScreenAddressInput')).typeText(
-      'Rue de Rivoli, 75001 Paris, France\n',
+      'test adress\n',
     );
 
     // Scroll to the submit button and tap it
@@ -35,7 +35,7 @@ describe('Login flow', () => {
     await element(by.id('assetCreationScreenSubmitButton')).tap();
 
     // Wait shortly for the asset to become visible in the asset list
-    await waitFor(element(by.text('Mona Lisa')))
+    await waitFor(element(by.text('test asset')))
       .toBeVisible()
       .withTimeout(5000);
 
@@ -50,10 +50,10 @@ describe('Login flow', () => {
       'Task test\n',
     );
     await element(by.id('assetSelectionButton')).tap();
-    await element(by.text('Mona Lisa')).tap();
+    await element(by.text('test asset')).tap();
 
     // Wait shortly for the asset to become visible in the asset list
-    await waitFor(element(by.text('Mona Lisa')))
+    await waitFor(element(by.text('test asset')))
       .toBeVisible()
       .withTimeout(5000);
     
@@ -65,6 +65,8 @@ describe('Login flow', () => {
     await element(by.text('Task test')).tap();
     await element(by.id('TaskDetailScreenMenuButton')).tap();
     await element(by.id('TaskEditButton')).tap();
+    await element(by.id('addSubTaskButton')).tap();
+    await element(by.id('subTaskInput')).typeText('help me again\n');
          // agin can't put a ref for a specific X button to delete a specific subtask
     await element(by.id('creationDoneButton')).tap();
 
@@ -74,6 +76,17 @@ describe('Login flow', () => {
 
     // Check that the task has been deleted
     await waitFor(element(by.text('Task test')))
+      .not.toBeVisible()
+      .withTimeout(5000);
+
+    // Open and delete the asset
+    await element(by.text('KULTURGÜTER')).tap();
+    await element(by.text('test asset')).tap();
+    await element(by.id('assetDetailScreenMenuButton')).tap();
+    await element(by.id('assetDetailScreenDeleteButton')).tap();
+
+    // Check that the asset has been deleted
+    await waitFor(element(by.text('test asset')))
       .not.toBeVisible()
       .withTimeout(5000);
 
