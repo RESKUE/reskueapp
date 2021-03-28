b9c127f53e3a09cfd394f23098fe3b0809d9eebd
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-28 18:56:57 +0200

    Increase the PinMarkers test coverage

--- __tests__/PinMarker.tests.js
+++ __tests__/PinMarker.tests.js
@@ -1,11 +1,35 @@
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
+import {Marker} from 'react-native-maps-osmdroid';
 import React from 'react';
 import PinMarker from '../src/components/PinMarker';
 
-it('pin marker renders correctly', () => {
-  const coordinate = {
-    latitude: 0,
-    longitude: 0,
-  };
+test('pin marker renders correctly', () => {
+  const coordinate = {latitude: 0, longitude: 0};
   render(<PinMarker coordinate={coordinate} />);
+});
+
+test('the pin markers callback', () => {
+  const onDrag = jest.fn();
+  const {UNSAFE_getByType} = render(
+    <PinMarker coordinate={{latitude: 0, longitude: 0}} onDrag={onDrag} />,
+  );
+
+  const dragCoord = {latitude: 42, longitude: 42};
+  fireEvent(UNSAFE_getByType(Marker), 'drag', {
+    nativeEvent: {coordinate: dragCoord},
+  });
+
+  expect(onDrag.mock.calls.length).toBe(1);
+  expect(onDrag.mock.calls[0][0]).toMatchObject(dragCoord);
+});
+
+test('the pin markers callback is optional', () => {
+  const {UNSAFE_getByType} = render(
+    <PinMarker coordinate={{latitude: 0, longitude: 0}} />,
+  );
+
+  const dragCoord = {latitude: 42, longitude: 42};
+  fireEvent(UNSAFE_getByType(Marker), 'drag', {
+    nativeEvent: {coordinate: dragCoord},
+  });
 });
