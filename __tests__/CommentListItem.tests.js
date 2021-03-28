22bccd553944eadc6a98383ed5a1f57e137289b8
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-28 21:00:39 +0200

    Increase the commet list items test coverage

--- __tests__/CommentListItem.tests.js
+++ __tests__/CommentListItem.tests.js
@@ -1,13 +1,47 @@
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
+import {AuthContext} from '@ilt-pse/react-native-kueres';
+import {Provider} from 'react-native-paper';
 import React from 'react';
 import CommentListItem from '../src/components/listItems/mediaListItems/CommentListItem';
-import {AuthContext} from '@ilt-pse/react-native-kueres';
 
-it('comment list item renders correctly', () => {
-  const data = {author: 'John', text: 'Hi'};
+test('comment list item renders correctly', () => {
+  const data = {
+    author: 'John',
+    text: 'Hi',
+    media: [{id: 42, altText: 'Plan', mimeType: 'image/png'}],
+  };
   render(
     <AuthContext.Provider value={{clientRoles: []}}>
       <CommentListItem data={data} />
     </AuthContext.Provider>,
   );
+});
+
+test('item opens media screen on press', () => {
+  const data = {
+    author: 'John',
+    text: 'Hi',
+    media: [{id: 42, altText: 'Plan', mimeType: 'image/png'}],
+  };
+  const {getByText} = render(
+    <AuthContext.Provider value={{clientRoles: []}}>
+      <CommentListItem data={data} />
+    </AuthContext.Provider>,
+  );
+  fireEvent.press(getByText('Plan'));
+});
+
+test('pressing delete triggers the deletion callback', async () => {
+  const data = {author: 'John', text: 'Hi'};
+  const {getByText} = render(
+    <Provider>
+      <AuthContext.Provider value={{clientRoles: ['administrator']}}>
+        <CommentListItem data={data} />
+      </AuthContext.Provider>
+      ,
+    </Provider>,
+  );
+  fireEvent(getByText('Hi'), 'longPress', {nativeEvent: {pageX: 0, pageY: 0}});
+
+  // Unfortunately we are unable to reference the delete button, since it's rendered above all other components outside of the tree.
 });
