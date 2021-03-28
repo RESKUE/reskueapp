584948e8dbc801d6e481ec6143ea794c324665b7
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-03-28 21:00:08 +0200

    Refactor and test the user selection list item

--- __tests__/UserSelectionListItem.tests.js
+++ __tests__/UserSelectionListItem.tests.js
@@ -1,8 +1,16 @@
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 import React from 'react';
 import UserSelectionListItem from '../src/components/listItems/UserSelectionListItem';
 
-it('user selection list item renders correctly', () => {
+test('user selection list item renders correctly', () => {
   const data = {id: 1, name: 'name'};
   render(<UserSelectionListItem data={data} />);
+});
+
+test('prassing triggers navigation', () => {
+  const data = {id: 1, name: 'name'};
+  const {getByTestId} = render(
+    <UserSelectionListItem testID="userItem" data={data} />,
+  );
+  fireEvent.press(getByTestId('userItem'));
 });
--- src/components/listItems/UserSelectionListItem.js
+++ src/components/listItems/UserSelectionListItem.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {List} from 'react-native-paper';
 
-export default function UserSelectionListItem({data}) {
+export default function UserSelectionListItem({testID, data}) {
   const navigation = useNavigation();
+
+  return (
+    <List.Item
+      testID={testID}
+      key={data.id}
+      title={data.name}
+      onPress={onPress}
+    />
+  );
 
   function onPress() {
     navigation.navigate('UsergroupCreationScreen', {
       userId: data.id,
     });
   }
-
-  return <List.Item key={data.id} title={data.name} onPress={onPress} />;
 }
