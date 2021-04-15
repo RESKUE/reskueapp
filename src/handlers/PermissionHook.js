afa373b69c08d1694a04ab9e8feb74a44cf000df
Jonathan Marcel Ehwald <uefsn@student.kit.edu>
2021-04-15 17:31:36 +0200

    Prevent multiple requests of the same permission

--- src/handlers/PermissionHook.js
+++ src/handlers/PermissionHook.js
@@ -3,8 +3,15 @@ import {PermissionsAndroid} from 'react-native';
 
 export default function usePermission(permission, title, message) {
   const [granted, setGranted] = React.useState(null);
+  const [requested, setRequested] = React.useState(false);
 
   const request = React.useCallback(async () => {
+    // Prevent that the same permission is asked multiple times due to screen reloads.
+    if (requested) {
+      return;
+    }
+    setRequested(true);
+
     try {
       const result = await PermissionsAndroid.request(permission, {
         title: title,
@@ -14,10 +21,10 @@ export default function usePermission(permission, title, message) {
       });
       setGranted(result === PermissionsAndroid.RESULTS.GRANTED);
     } catch (err) {
-      console.log("Error requesting permissions:", err);
+      console.log('Error requesting permissions:', err);
       setGranted(false);
     }
-  }, [permission, title, message, setGranted]);
+  }, [requested, permission, title, message, setGranted]);
 
   return {granted, request};
 }
